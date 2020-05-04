c5b05ff7bdbd3512a23c1e8d35243a035a94cce6
amueller <andreas.mueller@bcmsolutions.de>
2020-05-04 09:16:23 +0200

    Initial commit
    Initial commit with basic project structure

---
+++ PetClinicPersistenceService/src/main/java/am/training/PetClinicPersistenceService/PetClinicPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package am.training.PetClinicPersistenceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicPersistenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicPersistenceServiceApplication.class, args);
+	}
+
+}
---
+++ PetClinicPersistenceService/src/test/java/am/training/PetClinicPersistenceService/PetClinicPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package am.training.PetClinicPersistenceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicPersistenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
